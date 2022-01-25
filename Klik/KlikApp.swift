93dd3f19f3f57e4bf6b922d216ac2b92bcb6929a
Rostislav Brož <rostislav.broz@outlook.com>
2022-01-25 16:11:55 +0100

    Initial Commit

---
+++ Klik/KlikApp.swift
@@ -0,0 +1,17 @@
+//
+//  KlikApp.swift
+//  Klik
+//
+//  Created by Rostislav Brož on 1/25/22.
+//
+
+import SwiftUI
+
+@main
+struct KlikApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
