dd7729661bd530aef35408ded95e2affb74c6695
Rostislav Brož <rostislav.broz@outlook.com>
2022-01-25 16:19:03 +0100

    Create `ContentModel.swift`

---
+++ Klik/ViewModels/ContentModel.swift
@@ -0,0 +1,8 @@
+//
+//  ContentModel.swift
+//  Klik
+//
+//  Created by Rostislav Brož on 1/25/22.
+//
+
+import Foundation
--- Klik/Views/HomeView.swift
+++ Klik/Views/HomeView.swift
@@ -9,7 +9,13 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Button(action: {},
+                   label: {
+                        Rectangle()
+                            .fill()
+            }).ignoresSafeArea()
+        }
     }
 }
 
