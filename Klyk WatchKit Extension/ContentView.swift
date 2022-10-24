46339c565eb4b430d7656379d7e8898e9cea048e
Rostislav Brož <scraptechguy@gmail.com>
2022-10-24 09:34:12 +0200

    Create Apple Watch target

---
+++ Klyk WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Klyk WatchKit Extension
+//
+//  Created by Rostislav Brož on 10/24/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Klyk WatchKit Extension/NotificationView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationView.swift
+//  Klyk WatchKit Extension
+//
+//  Created by Rostislav Brož on 10/24/22.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
