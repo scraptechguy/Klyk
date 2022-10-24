4a1ac9cd3414f9cc2342662d2d5c490d94ae65c1
Rostislav Brož <scraptechguy@gmail.com>
2022-10-24 09:40:16 +0200

    Create Apple Watch target

---
+++ Klyk WatchKit Extension/KlikApp.swift
@@ -0,0 +1,19 @@
+//
+//  KlikApp.swift
+//  Klyk WatchKit Extension
+//
+//  Created by Rostislav Brož on 10/24/22.
+//
+
+import SwiftUI
+
+@main
+struct KlikApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
